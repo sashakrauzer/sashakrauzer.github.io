ba0c0309bfc38cba3d4a37c52c0772c2e58de30e
Aleksandr Gusev <alex-krauzer@yandex.ru>
2020-10-27 16:34:17 +0300

    added new elements2 page

---
+++ elements2/precache-manifest.108488a64a2c244130f8ca93b9d6e9d9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2b1e15f074eddaafb2c0984babfc005",
+    "url": "./index.html"
+  },
+  {
+    "revision": "23a721d7c106a08a56fc",
+    "url": "./static/css/2.99a5358a.chunk.css"
+  },
+  {
+    "revision": "3ccdda5c616e961ff36b",
+    "url": "./static/css/main.95f818c8.chunk.css"
+  },
+  {
+    "revision": "23a721d7c106a08a56fc",
+    "url": "./static/js/2.7f14536f.chunk.js"
+  },
+  {
+    "revision": "fb853b69041a1c67478a1d9066f60b75",
+    "url": "./static/js/2.7f14536f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ccdda5c616e961ff36b",
+    "url": "./static/js/main.0c4bcbf8.chunk.js"
+  },
+  {
+    "revision": "70290496268b8832178c",
+    "url": "./static/js/runtime-main.253f0d73.js"
+  },
+  {
+    "revision": "b15f934a6833c5f7c6013e2a3cbc6022",
+    "url": "./static/media/elements.b15f934a.svg"
+  }
+]);
