71b7156aece6d53808900ba9221f30dd47ebb31c
Aleksandr Gusev <alex-krauzer@yandex.ru>
2020-11-04 10:20:33 +0300

    updated

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.0ec581552a37fc675347a449b771bda8.js"
+  "./precache-manifest.a11ca6ebefe4d2af93f996f9d99e7bb8.js"
 );
 
 self.addEventListener('message', (event) => {
