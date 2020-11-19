f9e0c6563033d51c344730d8d00f522a35ba3329
Aleksandr Gusev <alex-krauzer@yandex.ru>
2020-11-19 10:49:38 +0300

    updated

---
+++ precache-manifest.1aaab734960fd942e48cf2062d8a17e9.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f98ae867d5db3d28afd737b0036ac94d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "97471966fb4297f09a43",
+    "url": "./static/css/2.99a5358a.chunk.css"
+  },
+  {
+    "revision": "c656449b7654f6e1d59b",
+    "url": "./static/css/main.b3821d69.chunk.css"
+  },
+  {
+    "revision": "97471966fb4297f09a43",
+    "url": "./static/js/2.204aa5e9.chunk.js"
+  },
+  {
+    "revision": "fb853b69041a1c67478a1d9066f60b75",
+    "url": "./static/js/2.204aa5e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c656449b7654f6e1d59b",
+    "url": "./static/js/main.2a0640b1.chunk.js"
+  },
+  {
+    "revision": "8174e045d8e45be4f01a209e48d910e4",
+    "url": "./static/js/main.2a0640b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70290496268b8832178c",
+    "url": "./static/js/runtime-main.253f0d73.js"
+  },
+  {
+    "revision": "b15f934a6833c5f7c6013e2a3cbc6022",
+    "url": "./static/media/elements.b15f934a.svg"
+  },
+  {
+    "revision": "d70766d0c6072da891b855f6e6ae1f86",
+    "url": "./static/media/facebook.d70766d0.svg"
+  }
+]);
